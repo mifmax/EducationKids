471fa779194d2edd75d46d1262dd345d2b71e178
mifmax <mifmax@tut.by>
2013-01-23 14:47:07 +0300

    draw

--- EducationKids/src/home/maximv/courses/magicletter/Draw.java
+++ EducationKids/src/home/maximv/courses/magicletter/Draw.java
@@ -1,7 +1,6 @@
 package home.maximv.courses.magicletter;
 
 import java.util.ArrayList;
-import java.util.List;
 
 import android.content.Context;
 import android.graphics.Canvas;
@@ -12,9 +11,12 @@ import android.view.MotionEvent;
 import android.view.View;
 
 public class Draw extends View {
-   private Paint paint = new Paint();
-   private List<Point> points = new ArrayList<Point>(); 
-   public Draw(Context context) {
+
+    private Paint paint = new Paint();
+    private ArrayList<Point> points = new ArrayList<Point>();
+    private ArrayList<ArrayList<Point>> paths =new ArrayList<ArrayList<Point>>();
+
+    public Draw(Context context) {
         super(context);
         setFocusable(true);
         setFocusableInTouchMode(true);
@@ -22,25 +24,44 @@ public class Draw extends View {
         paint.setAntiAlias(true);
     }
 
-    @Override
-    protected void onDraw(Canvas canvas){
-        //super.onDraw(canvas);
-        
-        paint.setColor(Color.BLACK);
-        for (Point point : points) {
-            canvas.drawCircle(point.x, point.y, 2, paint); 
-        }
+   @Override
+   public void onDraw(Canvas canvas){
+       super.onDraw(canvas);
+       //this.setBackgroundResource(R.drawable.picture_frame);
+       this.setBackgroundColor(Color.WHITE);
+       paint.setColor(Color.BLACK);
+       for(ArrayList<Point> path: paths){
+           drawPath(path, canvas);
+       }
+       drawPath(points, canvas);
     }
 
-   @Override
-   public boolean onTouchEvent(MotionEvent event) {
-       
-       Point point = new Point();
-       point.x = (int) event.getX();
-       point.y = (int) event.getY();
-       points.add(point); 
-
-       invalidate();
-    return true;
+   private void drawPath(ArrayList<Point> path, Canvas canvas){
+       if (path.size() > 0) {
+           Point point0 = path.get(0);
+           for (int i = 1; i < path.size(); i++) {
+               Point pointNext = path.get(i);
+               canvas.drawLine(point0.x, point0.y, pointNext.x, pointNext.y, paint);
+               point0 = pointNext;
+           }
+       }
    }
+
+   @Override
+   public boolean onTouchEvent(MotionEvent event){
+       switch (event.getActionMasked()) {
+           case MotionEvent.ACTION_MOVE:
+               points.add(new Point((int)event.getX(), (int)event.getY()));
+               invalidate();
+               break;
+           case MotionEvent.ACTION_UP:
+               paths.add(points);
+               points = new ArrayList<Point>();
+               break;
+           default:
+               break;
+       }
+       return true;
+   }
+
 }
