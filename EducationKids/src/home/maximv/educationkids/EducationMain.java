0116b3ecd4befe1153b228ad527be3ff29cbc8d2
mifmax <mifmax@tut.by>
2013-02-11 22:40:24 +0300

    1

--- EducationKids/src/home/maximv/educationkids/EducationMain.java
+++ EducationKids/src/home/maximv/educationkids/EducationMain.java
@@ -5,13 +5,7 @@ import home.maximv.db.vo.Learner;
 import home.maximv.utils.SpeechRecognition;
 import home.maximv.utils.WikiRequest;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.Reader;
-import java.io.StringWriter;
-import java.io.Writer;
 import java.net.URISyntaxException;
 import java.util.ArrayList;
 import java.util.List;
